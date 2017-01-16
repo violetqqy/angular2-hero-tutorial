bccfebe86ae98c6ede6aaff0cbbe2cf2ad48319d
violet_qqy <forviolet771@gmail.com>
2017-01-16 09:55:00 +0800

    finish tuorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,40 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import './rxjs-entensions';
+
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './heroes';
+
 import { AppComponent } from './app.component';
+import { 
+  HeroesComponent, 
+  HeroDetailComponent, 
+  DashboardComponent, 
+  HeroSearchComponent,
+  HeroService,
+} from './heroes';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule,
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroSearchComponent
+  ],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/heroes/index.ts
@@ -0,0 +1,6 @@
+export * from './hero.service';
+export * from './dashboard';
+export * from './hero-detail';
+export * from './heroes-list';
+export * from './hero-search';
+export * from './in-memory-data.service';
